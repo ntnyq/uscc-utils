3b670895ade55c513a825c82cf2a3102e2ccfefc
ntnyq <ntnyq13@gmail.com>
2024-07-24 23:58:50 +0800

    chore: update

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
-      reporter: ['lcov', 'text', 'clover', 'json'],
+      reporter: ['lcov', 'text', 'clover'],
     },
   },
 
