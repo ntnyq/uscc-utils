767f6c0d43ea6d439ce223c19bb2a37d2161a2aa
ntnyq <ntnyq13@gmail.com>
2024-11-08 15:59:50 +0800

    fix: refine jsdoc

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,31 @@
+/**
+ * Result of {@link parseUSCC}
+ */
 export interface ParseResult {
+  /**
+   * whether the USCC is valid
+   */
   isValid: boolean
+  /**
+   * category of the USCC
+   */
   category: string
+  /**
+   * type of the USCC
+   */
   type: string
 }
 
+/**
+ * Options of {@link parseUSCC}
+ */
 export interface ParseOptions {
+  /**
+   * placeholder for unknown category
+   */
   unknownCategory?: string
+  /**
+   * placeholder for unknown type
+   */
   unknownType?: string
 }
