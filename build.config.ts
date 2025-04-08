63df972c87284986de48474c561feaa7dfcaba97
ntnyq <ntnyq13@gmail.com>
2025-04-08 14:22:36 +0800

    feat!: drop CJS build, now it's ESM only

--- build.config.ts
+++ build.config.ts
@@ -2,10 +2,9 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   clean: true,
-  declaration: true,
+  declaration: 'node16',
   entries: ['src/index'],
   rollup: {
-    emitCJS: true,
     esbuild: {
       charset: 'utf8',
     },
